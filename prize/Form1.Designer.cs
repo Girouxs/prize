e7d807ff228fe26946d95f69dde12510f1f50cb4
Giroux <giroux.shen@unisco.com>
2017-01-12 10:30:24 +0800

    prize

---
+++ prize/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace prize
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(374, 318);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(254, 82);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(355, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 110);
+            this.label1.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.richTextBox1.Font = new System.Drawing.Font("微软雅黑", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.richTextBox1.Location = new System.Drawing.Point(697, 115);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(126, 285);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1016, 546);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Prize";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ prize/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace prize
+{
+    public partial class Form1 : Form
+    {
+        BackgroundWorker bw = null;
+
+        private string path;
+        private List<string> names;
+        private List<string> winnerNames;
+        private string prizeWinner;
+        private int nameIndex = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            path = Path.Combine(Application.StartupPath, "name.txt");
+            names = new List<string>();
+            winnerNames = new List<string>();
+            
+            getNames();
+
+            bw = new BackgroundWorker();
+            bw.WorkerReportsProgress = true;
+            bw.WorkerSupportsCancellation = true;
+            bw.DoWork += new DoWorkEventHandler(bw_dowork);
+            bw.ProgressChanged += new ProgressChangedEventHandler(bw_progress);
+            bw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bw_complete);
+        }
+
+        private void getNames()
+        {
+            StreamReader sr = new StreamReader(path, Encoding.UTF8);
+            String line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                line = line.Trim();
+                if (String.IsNullOrEmpty(line)) continue;
+                names.Add(line);
+                if (line.StartsWith("--"))
+                {
+                    winnerNames.Add(line.Substring(2));
+                    setWinner(line.Substring(2));
+                }
+            }
+            sr.Close();
+        }
+
+        private void setWinner(string name)
+        {
+            richTextBox1.Text += name + "\r\n";
+        }
+
+        private void updateName(string winner)
+        {
+            FileStream fs = new FileStream(path, FileMode.OpenOrCreate);
+            StreamWriter sw = new StreamWriter(fs, Encoding.UTF8);
+
+            for (int i = 0; i < names.Count; i++) {
+                if (names[i].Equals(winner))
+                {
+                    names[i] = "--" + names[i];
+                    sw.WriteLine(names[i]);
+                }
+                else {
+                    sw.WriteLine(names[i]);
+                }
+            }
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+
+            winnerNames.Add(winner);
+            setWinner(winner);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (winnerNames.Count == names.Count)
+            {
+                label1.Text = "over";
+                button1.Text = "over";
+                return;
+            }
+
+            if (button1.Text.Equals("start"))
+            {
+                button1.Text = "stop";
+                bw.RunWorkerAsync();
+            }
+            else {
+                button1.Text = "start";
+                bw.CancelAsync();
+                bw.Dispose();
+            }
+        }
+
+        //==============
+
+        private void bw_dowork(object sender, DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                if (winnerNames.Count == names.Count) return;
+
+                if (nameIndex >= names.Count) nameIndex = 0;
+                while (names[nameIndex].StartsWith("--"))
+                {
+                    nameIndex++;
+                    if (nameIndex >= names.Count) nameIndex = 0;
+                }
+                prizeWinner = names[nameIndex];
+                nameIndex++;
+
+                bw.ReportProgress(1);
+                System.Threading.Thread.Sleep(100);
+
+                if (bw.CancellationPending)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+            }
+        }
+
+        private void bw_progress(object sender, ProgressChangedEventArgs e)
+        {
+            label1.Text = prizeWinner;
+        }
+
+        private void bw_complete(object sender, RunWorkerCompletedEventArgs e)
+        {
+            updateName(prizeWinner);
+        }
+    }
+}
